7b4644d588c62f23b72dc39a115ef8349aa4e689
Járedy Alves <jaredyalves@undefinedname.com>
2024-05-12 19:06:17 +0100

    feat(fish): check for gpg command and add comments

--- roles/fish/files/conf.d/gpg-agent.fish
+++ roles/fish/files/conf.d/gpg-agent.fish
@@ -1,8 +1,14 @@
-set -x GPG_TTY (tty)
+# Check if gpg command exists
+if command -v gpg >/dev/null
+    # Set GPG_TTY environment variable to the current tty
+    set -x GPG_TTY (tty)
 
-set -e SSH_AGENT_PID
-if not set -q gnupg_SSH_AUTH_SOCK_by[1] || math $gnupg_SSH_AUTH_SOCK_by[1] -ne (pid)
-    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    # Ensure SSH_AGENT_PID is set and update SSH_AUTH_SOCK if necessary
+    if not set -q gnupg_SSH_AUTH_SOCK_by[1] || math $gnupg_SSH_AUTH_SOCK_by[1] -ne (pid)
+        # Update SSH_AUTH_SOCK using gpgconf if necessary
+        set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    end
+
+    # Update GPG agent's startup tty
+    gpg-connect-agent updatestartuptty /bye >/dev/null
 end
-
-gpg-connect-agent updatestartuptty /bye >/dev/null
