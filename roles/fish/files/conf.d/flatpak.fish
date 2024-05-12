52c00b47539038c11828a234b56892d21caddd66
Járedy Alves <jaredyalves@undefinedname.com>
2024-05-12 18:46:28 +0100

    feat(fish): check for flatpak bin and comments

--- roles/fish/files/conf.d/flatpak.fish
+++ roles/fish/files/conf.d/flatpak.fish
@@ -1,6 +1,14 @@
-set -ga fish_user_paths ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
-set -gx --path XDG_DATA_DIRS ~/.local/share/flatpak/exports/share
-for install_dir in (flatpak --installations)
-    set -gxa XDG_DATA_DIRS $install_dir/exports/share
+# Check if flatpak command exists
+if command -v flatpak >/dev/null
+    # Set paths for flatpak installations
+    set -ga fish_user_paths ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    set -gx --path XDG_DATA_DIRS ~/.local/share/flatpak/exports/share
+
+    # Add paths for each flatpak installation
+    for install_dir in (flatpak --installations)
+        set -gxa XDG_DATA_DIRS $install_dir/exports/share
+    end
+
+    # Add additional default paths
+    set -gxa XDG_DATA_DIRS /usr/local/share /usr/share
 end
-set -gxa XDG_DATA_DIRS /usr/local/share /usr/share
